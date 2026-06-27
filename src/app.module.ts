143dce058e323a88c6225d530bc5210bface590b
Mathías Terán <agusmaty23@gmail.com>
2026-06-26 22:24:19 -0500

    feat: initialize base project structure with user and subscription modules using TypeORM and SQLite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'better-sqlite3',
+    database: 'database.sqlite',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),SubscriptionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
