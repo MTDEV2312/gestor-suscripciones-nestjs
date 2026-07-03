94259fbdf7dc0e3f2ad397c2d07dfac41f9887c9
Mathías Terán <agusmaty23@gmail.com>
2026-07-02 21:55:23 -0500

    feat: initialize NestJS project structure with User entity and TypeORM configuration

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,48 +1,53 @@
-import { Subscription } from "src/subscriptions/entities/subscription.entity";
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Subscription } from 'src/subscriptions/entities/subscription.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
-
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({
-        type:'varchar',
-        length:20,
-        unique:true,
-        nullable:false,
-        comment:'nombre del usuario',
-    })
-    username:string;
+  @Column({
+    type: 'varchar',
+    length: 20,
+    unique: true,
+    nullable: false,
+    comment: 'nombre del usuario',
+  })
+  username: string;
 
-    @Column({
-        type:'varchar',
-        unique:true,
-        nullable:false,
-        comment:'correo electronico del usuario'
-    })
-    email:string;
+  @Column({
+    type: 'varchar',
+    unique: true,
+    nullable: false,
+    comment: 'correo electronico del usuario',
+  })
+  email: string;
 
-    @Column({
-        type:'varchar',
-        nullable:false,
-        comment:'contraseña del usuario'
-    })
-    password:string;
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    comment: 'contraseña del usuario',
+  })
+  password: string;
 
-    @CreateDateColumn({
-        type:'datetime',
-        comment:'fecha de registro del usuario'
-    })
-    createdAt:Date;
+  @CreateDateColumn({
+    type: 'datetime',
+    comment: 'fecha de registro del usuario',
+  })
+  createdAt: Date;
 
-    @CreateDateColumn({
-        type:'datetime',
-        comment:'fecha de actualizacion del usuario'
-    })
-    updatedAt:Date;
+  @CreateDateColumn({
+    type: 'datetime',
+    comment: 'fecha de actualizacion del usuario',
+  })
+  updatedAt: Date;
 
-    @OneToMany(()=>Subscription, (subscription) => subscription.user)
-    subscriptions:Subscription[];
+  @OneToMany(() => Subscription, (subscription) => subscription.user)
+  subscriptions: Subscription[];
 }
