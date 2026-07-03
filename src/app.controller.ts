72605bca33bc471e0900c431455990a112ded45a
Mathías Terán <agusmaty23@gmail.com>
2026-07-02 22:14:01 -0500

    feat: initialize core modules and user domain with SQLite integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
-  Health() {
+  health() {
     return this.appService.getHealth();
   }
 }
