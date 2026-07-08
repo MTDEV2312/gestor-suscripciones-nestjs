f77b40dd592d3ab9929a02f172653955e4a81276
Mathías Terán <agusmaty23@gmail.com>
2026-07-07 23:09:25 -0500

    feat: add JWT authentication strategy and guard for secure route access

---
+++ src/auth/strategies/jwt.strategy/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/users/users.service';
+
+interface jwtPayload {
+    id: string;
+    username: string;
+    email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly configService: ConfigService, private readonly userService: UsersService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET'),
+        });
+    }
+
+    async validate(payload: jwtPayload): Promise<jwtPayload> {
+        const user= await this.userService.findById(payload.id);
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
