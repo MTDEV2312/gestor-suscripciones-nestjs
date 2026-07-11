95dafdc319bef3075c88052ef4b44fd304951744
Mathías Terán <agusmaty23@gmail.com>
2026-07-10 19:21:40 -0500

    feat: remove unused dashboard DTOs to streamline codebase

--- src/dashboard/dto/dashboard-query.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { DashboardResponseDto } from './dashboard-response.dto';
-
-
-export class DashboardQueryDto extends PartialType(DashboardResponseDto) {}
